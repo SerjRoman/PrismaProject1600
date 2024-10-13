77b8c2fc9e34488b4a69ec08297622178470f460
SerjRoman <sergeyroman26@gmail.com>
2024-10-13 17:35:43 +0100

    Seeds added

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,59 @@
-import { PrismaClient } from "@prisma/client";
-
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
-async function main() {
-    const product = await prisma.product.update({
-        where: {
-            id: 3
-        },
+// Создание продукта 
+async function createProduct(){
+    const product = await prisma.product.create({
         data: {
-            name: "Super Updated Product"
+            name: 'First product',
+            price: 2
         }
     })
     console.log(product)
 }
+// Удаления продукта по id
+async function deleteProduct() {
+    const product = await prisma.product.delete({
+        where: {
+            id: 1
+        }
+    })
+    console.log(product)
+}
+// Обновления данных о продукте
+async function updateProduct() {
+    const product = await prisma.product.update({
+        where: {
+            id: 1
+        },
+        data: {
+            name: 'Updated Product'
+        }
+    })
+    console.log(product)
+
+}
+
+async function findProduct() {
+    const product = await prisma.product.findUnique({
+        where: {
+            id: 1
+        }
+    })
+    console.log(product)
+}
+
+async function main() {
+    await createProduct()
+    await findProduct()
+    await updateProduct()
+    await deleteProduct()
+}
+
 main().then(() => {
     prisma.$disconnect()
-}).catch((error) => {
+}).catch((err) => {
+    console.log(err)
     prisma.$disconnect()
-    console.log(error)
 })
-
